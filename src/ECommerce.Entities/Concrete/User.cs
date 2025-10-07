b5abec07b010b60a6a6633fd2b548c5944be1905
DilaraSara <114562131+DilaraSara@users.noreply.github.com>
2025-10-07 11:29:36 +0300

    update

--- src/ECommerce.Entities/Concrete/User.cs
+++ src/ECommerce.Entities/Concrete/User.cs
@@ -4,23 +4,20 @@ using System.Collections.Generic;
 namespace ECommerce.Entities.Concrete
 {
     public class User : IdentityUser<int>
-{
-    // Id, Email, PasswordHash zaten IdentityUser<int>’den geliyor, kaldır
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string FullName => $"{FirstName} {LastName}";
-    //public bool IsCourier { get; set; }
-    public string? Address { get; set; }
-    public string? City { get; set; }
-    public string Role { get; set; } = "User";
-    public bool IsActive { get; set; } = true;
-    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    public DateTime? UpdatedAt { get; set; }
-    public string? PasswordResetToken { get; set; }
-    public DateTime? ResetTokenExpires { get; set; }
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string FullName { get; set; } = string.Empty; // testte atama yapılabiliyor
+        public string? Address { get; set; }
+        public string? City { get; set; }
+        public string Role { get; set; } = "User";
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public string? Password { get; set; } // test için eklendi
+        public string? PasswordResetToken { get; set; }
+        public DateTime? ResetTokenExpires { get; set; }
 
-    // Navigation Properties
         public virtual ICollection<Order> Orders { get; set; } = new HashSet<Order>();
-}
-
+    }
 }
