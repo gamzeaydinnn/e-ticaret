3977867bf26e700549cb48e569d866f450a4159d
DilaraSara <114562131+DilaraSara@users.noreply.github.com>
2025-10-19 21:41:01 +0300

    update

---
+++ src/ECommerce.Entities/Enums/MovementType.cs
@@ -0,0 +1,12 @@
+namespace ECommerce.Entities.Enums
+{
+    public enum MovementType
+    {
+        Purchase = 0,
+        Sale = 1,
+        Return = 2,
+        Adjustment = 3,
+        Reservation = 4,
+        Release = 5
+    }
+}
